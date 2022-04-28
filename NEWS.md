a232b1898a26dfc807b301f56180e0ffff807af3
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2022-04-28 12:07:00 +0200

    Bump package version

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,5 @@
+# tesselle 1.0.0.9000
+
 # tesselle 1.0.0
 
 * First CRAN release.
--- R/tesselle-package.R
+++ R/tesselle-package.R
@@ -2,7 +2,7 @@
 #'  \tabular{ll}{
 #'   **Package:** \tab tesselle \cr
 #'   **Type:** \tab Package \cr
-#'   **Version:** \tab 1.0.0 \cr
+#'   **Version:** \tab 1.0.0.9000 \cr
 #'   **License:** \tab GPL-3 \cr
 #'  }
 #'
@@ -19,10 +19,10 @@
 #'  Nicolas Frerebeau\cr
 #'  \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
 #'
-#'  IRAMAT-CRP2A (UMR 5060)\cr
+#'  Archéosciences Bordeaux (UMR 6034)\cr
 #'  Maison de l'Archéologie\cr
 #'  Université Bordeaux Montaigne\cr
-#'  F-33607 Pessac cedex\cr
+#'  33607 Pessac cedex\cr
 #'  France
 #' @name tesselle-package
 #' @aliases tesselle
